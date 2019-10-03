c0002e71f208e4447d3c89aec8aa12dcb7d8591b
Matt Revell <mattrevell82@me.com>
2019-10-03 13:30:44 +0100

    adds AboutPage title attribute to a tag

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Index = () => (
   <div>
     <Link href="/about">
-      <a>About Page</a>
+      <a title="About Page">About Page</a>
     </Link>
     <p>Hello Next.js</p>
   </div>
